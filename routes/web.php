c92035c56acc01a4cdfa04b4596086c905cede31
TomoyaSuzuki-TAITO <tomoya.suzuki@creare-net.co.jp>
2026-05-27 23:56:13 +0900

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessagesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// index: showの補助ページ
+Route::get('/', [MessagesController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('messages', MessagesController::class);
